c84796f97dc96d9f14b76fc3dbc03d62c323dd95
Liza21l <Liza2009koshenya@gmail.com>
2024-12-10 17:26:25 +0200

    Create text.py

---
+++ text.py
@@ -0,0 +1,37 @@
+#####Task3
+# western_hemisphere = ['Північна Америка', 'Південна Америка']
+# western_hemisphere.extend('Євразія, Африка, Австралія, Антарктида'.split(','))
+# western_hemisphere.sort()
+# print(f"Список материків за алфавітом:")
+# for continent in western_hemisphere: 
+#     print(continent)
+
+#########Task4
+
+# students = [ ["Андрій", "Попов"], ["Марія", "Петрова"], ["Андрій", "Милий"], ["Олексій", "Геращенко"] ]
+# count = 0
+# for student in students:
+#     if student[0] == "Андрій":
+#         count +=1
+# print("Кількість студентів з іменем 'Андрій':", count)
+
+##########Task1
+
+# sentence = "привіт, як твої справи"  
+# words = sentence.split() 
+# for word in enumerate(words, start=1): 
+#     print(word)
+
+######Task2
+
+# number_list = [1, 2, 2, 3, 4, 4, 5]
+# unique_elements = []
+# indices = []
+# for index, element in enumerate(number_list):
+#     if element not in unique_elements:
+#         unique_elements.append(element)
+#         indices.append(index)
+
+# print("Новий список з унікальними елементами:", unique_elements)
+# print("Індекси перших входжень у початковому списку:", indices)
+
